f4de446064a9d74b9953b1ec5169f8f6740d97a2
prost p <prost_p@epitech.eu>
2014-12-15 01:04:23 +0100

    Container supportant l'installation d'une suite d'applications et leur execution sur demande

---
+++ docker-ecoledufutur/controller.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import BaseHTTPServer
+import json
+import subprocess, os
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+sys.stderr.write("{}".format(os.environ))
+
+with open("/var/packages") as json_file:
+    json_data = json.load(json_file)
+    print json_data
+    
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        if s.path == "/":
+            s.wfile.write(json.dumps(json_data))
+        if s.path.split("/")[1] == "run":
+            print("run " + s.path.split("/")[2] + " [{}]".format(json_data["installed"][s.path.split("/")[2]]))
+            subprocess.Popen(json_data["installed"][s.path.split("/")[2]])
+
+print("Serving on port 8000")
+
+server_class = BaseHTTPServer.HTTPServer
+httpd = server_class(("0.0.0.0", 8000), MyHandler)
+try:
+    httpd.serve_forever()
+except:
+    pass
